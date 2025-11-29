2a358f7ffeae35b1785471bf65e692a150a2927c
GabrielCanlas <gabrielcanlasoct@gmail.com>
2025-11-29 20:39:39 +0800

    Update if_statement.php

--- if_statement.php
+++ if_statement.php
@@ -1,49 +1,9 @@
-<?php
-$name = "Gabriel";
-$greeting = "Hello!";
+<?php include "includes/header.php";
 
-if ($name != "") {
-    $greeting = "Welcome back, " . $name;
+$stock = 10;
+
+if ($stock > 0) {
+    echo "<p>We have Pancit Canton available!</p>";
 }
 
-// Pancit Canton Stocks
-$pancitSpicyStocks = 5;
-$pancitCalamansiStocks = 3;
-$pancitSweetStocks = 5;
-$pancitOriginalStocks = 2;
-
-// Check availability
-if ($pancitSpicyStocks > 0) {
-    $pancitSstock = "In Stock";
-}
-
-if ($pancitCalamansiStocks > 0) {
-    $pancitCstock = "In Stock";
-}
-
-if ($pancitSweetStocks > 0) {
-    $pancitSWstock = "In Stock";
-}
-
-if ($pancitOriginalStocks > 0) {
-    $pancitOstock = "In Stock";
-}
-
-?>
-<!DOCTYPE html>
-<html>
-    <?php include_once 'includes/header.php'; ?>
-<body>
-    <h2><?= $greeting ?></h2>
-
-    <p>Pancit Canton Spicy: <?= $pancitSstock ?></p>
-
-    <p>Pancit Canton Calamansi: <?= $pancitCstock ?></p>
-
-    <p>Pancit Canton Sweet: <?= $pancitSWstock ?></p>
-
-    <p>Pancit Canton Original: <?= $pancitOstock ?></p>
-
-    <?php include_once 'includes/footer.php'; ?>
-</body>
-</html>
+include "includes/footer.php"; ?>
